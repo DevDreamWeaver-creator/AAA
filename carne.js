38ec9744529b73f62a29e4e2906902af01c8d01e
DevDreamWeaver-creator <146996054+DevDreamWeaver-creator@users.noreply.github.com>
2023-10-04 20:53:38 +0100

    Update carne.js

--- carne.js
+++ carne.js
@@ -1,7 +1,7 @@
-var santiago_idade = 12
-var alexandre_idade = 16
+var alguém_idade = 12
+var outroalguém_idade = 16
 
-if(santiago_idade == alexandre_idade){
+if(alguém_idade == outroalguém_idade){
     console.log("Têm as mesmas idades")
 }else{
     console.log("Eles tem idades diferentes")
