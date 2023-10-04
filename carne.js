753d91ee8dd8ce2139f4832174c76a153ac4060e
DevDreamWeaver-creator <146996054+DevDreamWeaver-creator@users.noreply.github.com>
2023-10-04 20:51:59 +0100

    Add files via upload

---
+++ carne.js
@@ -0,0 +1,8 @@
+var santiago_idade = 12
+var alexandre_idade = 16
+
+if(santiago_idade == alexandre_idade){
+    console.log("Têm as mesmas idades")
+}else{
+    console.log("Eles tem idades diferentes")
+}
